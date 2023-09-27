5a587d0fd0918e48f4b2c500b44ecc63af277efd
CaraChaato <pedrovinic12@gmail.com>
2023-09-27 11:08:38 -0300

    Estabelecendo conexão com o banco de dados

---
+++ Code/PostgreSQLConnection.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLConnection {
+    private static PostgreSQLConnection instance; // Instancia de Banco de Dados
+    private Connection connection; // Conectar o Banco 
+
+    private final String url = "salt.db.elephantsql.com"; // Url do Banco
+    private final String username = "pdhubmnq"; // Username do Banco
+    private final String password = "5rvDta-Ic9bjXJzKgfP4MW5zFFhQwiw5"; // Senha do Banco
+
+    // Cria uma conexão com o Banco de Dados
+    private PostgreSQLConnection() {
+        try { // Tenta Conectar
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password); 
+        } catch (SQLException | ClassNotFoundException e) { // Se não conseguir da erro
+            System.err.println("Error connecting to database: " + e.getMessage());
+        }
+    }
+
+    public static synchronized PostgreSQLConnection getInstance() {
+        if (instance == null) {
+            instance = new PostgreSQLConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Conexão fechada com sucesso.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar conexão: " + e.getMessage());
+        }
+    }
+}
