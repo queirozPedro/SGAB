78718e82eb0e13ba5c83d7a3ee72e0acd0d66128
LilPuppet <laviniadantass@gmail.com>
2023-09-29 09:27:40 -0300

    comentários didáticos

--- Code/Livro.java
+++ Code/Livro.java
@@ -55,18 +55,18 @@ public class Livro {
      * @param id
      * @return Livro
      */
-    public static Livro BuscaLivroId(int id) {
-        try (Connection connection = PostgreSQLConnection.getInstance().getConnection()) {
-            String query = "Select * from livro where idLivro = ?";
-            PreparedStatement state = connection.prepareStatement(query);
-            state.setInt(1, id);
-            ResultSet result = state.executeQuery();
-            while (result.next()) {
+    public static Livro BuscaLivroId(int id) { //Usado pelos administradores, para alteração, listagem e remoção
+        try (Connection connection = PostgreSQLConnection.getInstance().getConnection()) {//se a conexão funcionar
+            String query = "Select * from livro where idLivro = ?"; //cria a query, que é a pesquisa que iremos fazer
+            PreparedStatement state = connection.prepareStatement(query); //cria o state, aquele que executa a pesquisa
+            state.setInt(1, id); //preenche os ? com as informações desejadas
+            ResultSet result = state.executeQuery(); //recebe a tabela com as respostas da pesquisa
+            while (result.next()) {// enquanto houverem respostas, imprima-as
                 return new Livro(result.getInt(1), result.getString(2), result.getString(3), result.getString(4),
                         result.getString(5), result.getString(6), result.getString(7), result.getString(8),
                         result.getBoolean(9), result.getBoolean(10));
             }
-        } catch (Exception e) {
+        } catch (Exception e) {//se der erro, mostre qual foi
             System.out.println(e);
         }
         return null;
@@ -77,11 +77,11 @@ public class Livro {
         System.out.println("Por que parametro deseja buscar o livro:\n1-Titulo;\n2-Autor;\n3-Genero;\n0-Voltar;");
         int op = sc.nextInt();
         sc.nextLine();
-        switch (op) {
+        switch (op) {//escolhendo o tipo de busca, que será usada na pesquisa posteriormente
             case 1:
                 tipo = "titulo";
                 System.out.print("Titulo: ");
-                busca = sc.nextLine();
+                busca = sc.nextLine();//recebendo o que queremos buscar
                 break;
             case 2:
                 tipo = "autor";
@@ -100,8 +100,8 @@ public class Livro {
 
         try (Connection connection = PostgreSQLConnection.getInstance().getConnection()) {
 
-            String query = "Select * from livro where "+ tipo +" = ?"; // Busca no banco de dados
-            PreparedStatement state = connection.prepareStatement(query);
+            String query = "Select * from livro where "+ tipo +" = ?"; // Busca no banco de dados, neste caso, já que o ? é substituido por
+            PreparedStatement state = connection.prepareStatement(query); // 'algo', usamos a varivavel diretamente para a pesquisa ficar correta
             state.setString(1, busca);
             ResultSet result = state.executeQuery();// Resultados da execução da query.
             
