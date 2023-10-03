117ac20d34eccac41005ba58cd4c704e497bfefa
CaraChaato <pedrovinic12@gmail.com>
2023-10-03 09:01:49 -0300

    metodo insere cliente e correções no bd

--- Arquivos/BancoSGAB.sql
+++ Arquivos/BancoSGAB.sql
@@ -2,7 +2,7 @@ CREATE TABLE Usuario (
     cpf VARCHAR(11) NOT NULL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     senha VARCHAR(50) NOT NULL,
-    email VARCHAR(75) NOT NULL
+    email VARCHAR(75) NOT NULL,
 );
 
 CREATE TABLE Telefone(
@@ -46,9 +46,3 @@ CREATE TABLE Emprestimo (
     FOREIGN KEY (idLivro) REFERENCES Livro(idLivro)
 );
 
--- CREATE TABLE Penalizacao(
---     idCliente Serial,
---     motivoPenalizacao VARCHAR(100) NOT NULL,
---     FimPenalizacao Date NOT NULL,
---     FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
--- )
--- Code/Cliente.java
+++ Code/Cliente.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
+import java.sql.*;
 public class Cliente extends Usuario {
     private int idCliente;
 
@@ -23,6 +21,57 @@ public class Cliente extends Usuario {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    /**
+     * Falta revisar esse método
+     * @param nome
+     * @param email
+     * @param senha
+     * @param cpf
+     * @return
+     */
+    public static Usuario registroCliente(String nome, String email, String senha, String cpf) {
+        try (Connection connection = PostgreSQLConnection.getInstance().getConnection()) {
+           
+            String query = "SELECT * FROM usuario WHERE email = ? OR cpf = ?";
+            PreparedStatement state = connection.prepareStatement(query);
+            state.setString(1, email);
+            state.setString(2, cpf);
+            ResultSet result = state.executeQuery();
+        
+            if (result.next()) {
+                //um usuario com o mesmo cpf ou email já existem
+                System.out.println("um usuario com um mesmo cpf ou email já existem");
+    
+                // modificação dos dados do usuário que já existem
+                String updateQuery = "UPDATE usuario SET nome = ?, senha = ? WHERE email = ? OR cpf = ?";
+                PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+                updateStatement.setString(1, nome);
+                updateStatement.setString(2, senha);
+                updateStatement.setString(3, email);
+                updateStatement.setString(4, cpf);
+                updateStatement.executeUpdate();
+    
+                // retorna um usuario modificado
+                return new Usuario(cpf, nome, email, senha);
+            }
+    
+            // caso não tenha um usuario cm o msm nome ou cpf, continua o registro
+            String insertQuery = "INSERT INTO usuario (cpf, nome, email, senha) VALUES (?, ?, ?, ?)";
+            PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
+            insertStatement.setString(1, cpf);
+            insertStatement.setString(2, nome);
+            insertStatement.setString(3, email);
+            insertStatement.setString(4, senha);
+            insertStatement.executeUpdate();
+    
+            // registro ok, retorna um novo usuario 
+            return new Usuario(cpf, nome, email, senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
