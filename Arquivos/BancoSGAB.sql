6a6ab91661e6b52bb2345e6b188a90594cf78841
CaraChaato <pedrovinic12@gmail.com>
2023-10-04 16:17:11 -0300

    Correções no SQL

--- Arquivos/BancoSGAB.sql
+++ Arquivos/BancoSGAB.sql
@@ -7,21 +7,42 @@ CREATE TABLE Usuario (
 
 CREATE TABLE Telefone(
     idTelefone Serial PRIMARY KEY,
-    cpf VARCHAR(11),
+    cpf VARCHAR(11) NOT NULL,
     numero VARCHAR(11) NOT NULL,
     FOREIGN KEY (cpf) REFERENCES Usuario(cpf)
 );
 
 CREATE TABLE Adm (
     idAdm Serial PRIMARY KEY,
-    cpf VARCHAR(11),
+    cpf VARCHAR(11) NOT NULL,
     FOREIGN KEY (cpf) REFERENCES Usuario(cpf)
 );
 
 CREATE TABLE Cliente (
     idCliente Serial PRIMARY KEY,
-    cpf VARCHAR(11),
+    cpf VARCHAR(11) NOT NULL,
     FOREIGN KEY (cpf) REFERENCES Usuario(cpf)
+);
+
+CREATE TABLE LivroEmprestado(
+    idEmprestado Serial PRIMARY KEY,
+    idLivro int NOT NULL,
+    FOREIGN KEY(idLivro) REFERENCES Livro(idLivro)
+);
+
+CREATE TABLE DataEmprestimo(
+    idDataEmprestimo Serial PRIMARY KEY,
+    DataEmp Date NOT NULL
+);
+
+CREATE TABLE DataPrevista(
+    idDataPrevista Serial PRIMARY KEY,
+    DataPre Date NOT NULL
+);
+
+CREATE TABLE DataDevolucao(
+    idDataDevolucao Serial PRIMARY KEY,
+    DataDev Date NOT NULL
 );
 
 CREATE TABLE Livro (
@@ -38,12 +59,13 @@ CREATE TABLE Livro (
 );
 
 CREATE TABLE Emprestimo (
-    cpf VARCHAR(11),
-    idLivro Serial,
-    DataEmprestimo Date NOT NULL,
-    DataPrevista Date NOT NULL,
-    DataDevolucao Date NOT NULL,
-    FOREIGN KEY (cpf) REFERENCES Usuario(cpf),
-    FOREIGN KEY (idLivro) REFERENCES Livro(idLivro)
+    cpf VARCHAR(11) NOT NULL,
+    idEmprestado int NOT NULL,
+    idDataEmprestimo int NOT NULL,
+    idDataPrevista int NOT NULL,
+    idDataDevolucao int NOT NULL,
+    FOREIGN KEY(idEmprestado) REFERENCES LivroEmprestado(idEmprestado),
+    FOREIGN KEY(idDataEmprestimo) REFERENCES DataEmprestimo(idDataEmprestimo),
+    FOREIGN KEY(idDataPrevista) REFERENCES DataPrevista(idDataPrevista),
+    FOREIGN KEY(idDataDevolucao) REFERENCES DataDevolucao(idDataDevolucao)
 );
-
