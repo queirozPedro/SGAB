134db3a440e74d460b83d56697b7d77dd0f234d8
CaraChaato <pedrovinic12@gmail.com>
2023-10-20 09:22:40 -0300

    Consertando Emprestimo (de novo)

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -40,7 +40,7 @@ public class Emprestimo {
 
         if (emprestimos.size() < 3) {
             Livro livro = Livro.buscaLivroId(getIdLivro());
-            if (livro.getQuantDisponivel() > livro.getQuantEmprestados()) {
+            if (livro.getQuantLivros() >= livro.getQuantEmprestados() || livro.getQuantLivros() > 1) {
                 try {
                     String query = "INSERT Into emprestimo (cpf, idLivro, dataEmprestimo, dataPrevista) VALUES (?, ?, ?, ?)";
                     state = connection.prepareStatement(query);
