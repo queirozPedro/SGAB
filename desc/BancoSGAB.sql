f5375d708587c91301c03a1967fec6b2949cf6ca
CaraChaato <pedrovinic12@gmail.com>
2023-10-19 21:27:10 -0300

    Alterações no BD

--- desc/BancoSGAB.sql
+++ desc/BancoSGAB.sql
@@ -22,7 +22,8 @@ CREATE TABLE Livro (
     Editora VARCHAR(255) NOT NULL,
     ISBN VARCHAR(13) NOT NULL, 
     quantLivros int NOT NULL,
-    quantDisponivel int NOT NULL
+    quantDisponivel int NOT NULL,
+    quantEmprestados int NOT NULL
 );
 
 CREATE TABLE Emprestimo (
